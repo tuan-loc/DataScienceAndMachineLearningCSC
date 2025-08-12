fd3302b13f6511de40c448a59360d325dbe5c26c
tuanloc <tuanlocvocong@gmail.com>
2025-08-12 00:30:28 -0700

    completed day01

---
+++ Module01_FundamentalsOfPython/Day01/b02_02_str.py
@@ -0,0 +1,21 @@
+hoTen = 'Lê Anh Thư'
+d = len(hoTen)
+ktDT = hoTen[0] #--> 'L'
+ktVT3 = hoTen[3] #--> 'A'
+
+ktCuoi1 = hoTen[d-1]
+ktCuoi2 = hoTen[-1]
+
+ho = hoTen[0:2]
+ho2 = hoTen[:2]
+tenLot = hoTen[3:6]
+ten = hoTen[7:]
+
+strSo = '123456789'
+strSoLe = strSo[0::2] # --> '13579'
+strSoChan = strSo[1::2] # --> '2468'
+
+strGachNgang = '-' * 50
+print(strGachNgang)
+
+print()
